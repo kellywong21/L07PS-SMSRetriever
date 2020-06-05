d39c3a472535e0bc5abcbc946998aa0802e8eca0
18004885 <18004885@myrp.edu.sg>
2020-06-05 12:36:56 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/l07ps_smsretriever/FragmentSent.java
@@ -0,0 +1,28 @@
+package sg.edu.rp.c346.l07ps_smsretriever;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentSent extends Fragment {
+
+    public FragmentSent() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sent, container, false);
+    }
+}
