048e44c431e87ed7a0b23ed397ae834bc51ce29c
18004885 <18004885@myrp.edu.sg>
2020-06-05 15:05:01 +0800

    final

--- app/src/main/java/sg/edu/rp/c346/l07ps_smsretriever/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/l07ps_smsretriever/MainActivity.java
@@ -1,7 +1,13 @@
 package sg.edu.rp.c346.l07ps_smsretriever;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.PermissionChecker;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
+import android.content.ContentResolver;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        Fragment f1 = new FragmentNumber();
+        ft.replace(R.id.frame1,f1);
+
+        Fragment f2 = new FragmentWord();
+        ft.replace(R.id.frame2,f2);
+
+        ft.commit();
+
+
     }
 }
